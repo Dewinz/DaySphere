a2323dbe11a04dfaf7b18876ddeaf7f2b80ecf84
Gopi200 <61714236+Gopi200@users.noreply.github.com>
2023-08-26 16:40:24 +0200

    #0002 Finished; #0005 Started.
    Got rid of flutter framework, fixed up python parts to work with CUDA. Intial commit for #0005.

--- satis_planner/python-modules/testmod/testfile.py
+++
@@ -1,5 +0,0 @@
-def testfunc(arg: str):
-    if  (arg==None):
-        return "Import success"
-    else:
-        return arg + "is megastink"
--- speech_recognition_test/android/app/src/main/kotlin/com/example/speech_recognition_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.speech_recognition_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
