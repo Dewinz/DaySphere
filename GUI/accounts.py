b6bd863e016d6da6522c612b1b47b0532ef43b43
Dewinz <123136676+Dewinz@users.noreply.github.com>
2023-09-23 17:46:06 +0200

    Terminal Page.
    Made the terminal page, it has autocorrect, although it is not as desired because of framework limitations.
    Also a start on reformatting the code and adding the sidebar.

--- GUI/accounts.py
+++ GUI/accounts.py
@@ -1,125 +1,5 @@
 import customtkinter
-from communication.client import login, create_account, save_remember_me, establish_connection
-from PIL import ImageTk, Image
-import os
-from GUI.terminal import TerminalPageFrame
-
-# TODO
-# Refactor the naming to more concise.
-# - MainPage should be MainPage.
-# For example the LoginPage isn't really a page but a menu.
-# It's parent, LoginPageFrame however is a page.
-# Accessibility page?
-# Backgrounds?
-
-# Notes:
-# For password hiding the assets open_white.png is being used universally since open_black.png doens't look like what it's supposed to.
-# remembered is the variable to determine if client side should immediately go to MainPage or LoginPageFrame.
-# remembered should be changed to read a json file to immediately log in.
-remembered = False
-
-
-# ============ Back-End ============
-
-# (TEMPORARY) login logic to bypass username and password.
-def admin_login(master):
-    print("Admin log in attempted")
-    master.switch_view(MainPage)
-
-
-# Parameters cannot be passed through since it'll only run the function once then.
-# Therefore we are using a global variable to get the value.
-# Preferably this would be changed to be integrated into the class so that it doesn't needlessly take memory.
-box_state = False
-def checkbox_event():
-    global box_state
-    if box_state : box_state = False
-    else : box_state = True
-    print(box_state)
-
-
-# Toggles the password between hidden and shown on LoginPage.
-def toggle_hidden_login(app_instance):
-    if app_instance.pass_entry.cget("show") == '':
-        app_instance.pass_entry.configure(show='*')
-        app_instance.hide_pass_button.configure(image=customtkinter.CTkImage(light_image=Image.open("assets/hidden_white.png"),
-                                                                             dark_image=Image.open("assets/hidden_black.png")))
-    else:
-        app_instance.pass_entry.configure(show='')
-        app_instance.hide_pass_button.configure(image=customtkinter.CTkImage(light_image=Image.open("assets/open_white.png"),
-                                                                             dark_image=Image.open("assets/open_white.png")))
-
-
-# Toggles the password between hidden and shown on AccountCreationPage.
-def toggle_hidden_creation(app_instance):
-    if app_instance.pass_entry.cget("show") == '':
-        app_instance.pass_entry.configure(show='*')
-        app_instance.pass_ver_entry.configure(show='*')
-        app_instance.hide_pass_button.configure(image=customtkinter.CTkImage(light_image=Image.open("assets/hidden_white.png"),
-                                                                             dark_image=Image.open("assets/hidden_black.png")))
-    else:
-        app_instance.pass_entry.configure(show='')
-        app_instance.pass_ver_entry.configure(show='')
-        app_instance.hide_pass_button.configure(image=customtkinter.CTkImage(light_image=Image.open("assets/open_white.png"),
-                                                                             dark_image=Image.open("assets/open_white.png")))
-
-
-def password_verification(app_instance):
-    # TODO
-    # Make an error appear rather than outright disabling the button.
-    
-    # Horrid line of code, wish I could wrap the text.
-    if app_instance.password.get() == app_instance.ver_password.get() and app_instance.username.get() != "" and app_instance.password.get() != "" and app_instance.ver_password.get() != "" and len(app_instance.username.get()) > 5  and len(app_instance.password.get()) > 5  and len(app_instance.ver_password.get()) > 5:
-        app_instance.create_account_button.configure(state="normal")
-    else:
-        app_instance.create_account_button.configure(state="disabled")
-
-    
-# =========== Front-End ===========
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-        
-        # Sets the titlebar icon.
-        self.iconpath = ImageTk.PhotoImage(file=os.path.join("assets","logo@4x.png"))
-        self.wm_iconbitmap()
-        self.iconphoto(False, self.iconpath)
-        
-        # Sets the titlebar name.
-        self.title("DaySphere")
-        
-        # Defines the on boot up resolution, would like to change it to be dynamic based off of previous session.
-        # AKA if closed on second screen while windowed last session, keep it the same on next startup.
-        self.geometry("1000x750")
-
-        # Define a private variable which contains the view.
-        self._view = None
-        
-        # If you picked "remember me" on last sesssion while logging in immediately try to log in.
-        # It currently will immediately go to MainPage.
-        # But it should first actually try to log in by the server, then go to the MainPage.
-        if remembered : self.switch_view(MainPage)
-        else : self.switch_view(LoginPageFrame)
-        
-
-    def switch_view(self, view):
-        # Destroys current frame and replaces it with a new one.
-        new_view = view(self)
-        if self._view is not None:
-            self._view.destroy()
-        self._view = new_view
-        
-        # Makes it so the frame (which we call view) is allowed to take up the entire screen.
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        
-        # Rewrites the view onto the screen.
-        # Sticky makes the view take up the entire screen.
-        if view == MainPage:
-            self._view.grid(sticky="nesw")
-        else:
-            self._view.grid(sticky="nesw")
-
+from GUI.main import MainPage
 
 # Likely the first screen you see on startup, unless if "remembere me" was True, where you should be send to MainPage.
 class LoginPage(customtkinter.CTkFrame):
@@ -308,54 +188,55 @@ class ErrorLabel(customtkinter.CTkFrame):
         self.error_label.grid(row=0, column=0)
 
 
-class SettingsPage(customtkinter.CTkFrame):
-    def __init__(self, master):
-        customtkinter.CTkFrame.__init__(self, master)
-        
-        # TODO
-        # The logout_button should also log out server side.
-        
-        self.acc_creation_label = customtkinter.CTkLabel(self, text="Settings", font=customtkinter.CTkFont(self, size=20))
-        self.acc_creation_label.grid(row=0, column = 0, padx=10, pady=(20, 0))
-        
-        self.logout_button = customtkinter.CTkButton(self, text="Log out", command=lambda: master.master.switch_view(LoginPageFrame))
-        self.logout_button.grid(row=1, column=0, padx=10, pady=20)
+# (TEMPORARY) login logic to bypass username and password.
+def admin_login(master):
+    print("Admin log in attempted")
+    master.switch_view(MainPage)
 
 
-# Boxes our view SettingsPage into a nice Frame, which is the border containing all LoginPage elements.
-# So the Frame takes up the entire view, and the SettingsPage is centered inside.
-# SettingsPage's parent is SettingsPageFrame, therefore master.master is used in SettingsPage.
-class SettingsPageFrame(customtkinter.CTkFrame):
-    def __init__(self, master):
-        customtkinter.CTkFrame.__init__(self, master, fg_color="transparent")
-        
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        
-        self.account_creation_page = SettingsPage(self)
-        self.account_creation_page.grid(row=1, column=0)
+# Parameters cannot be passed through since it'll only run the function once then.
+# Therefore we are using a global variable to get the value.
+# Preferably this would be changed to be integrated into the class so that it doesn't needlessly take memory.
+box_state = False
+def checkbox_event():
+    global box_state
+    if box_state : box_state = False
+    else : box_state = True
+    print(box_state)
 
 
-# The MainPage where all functions can be found and navigated to.
-class MainPage(customtkinter.CTkFrame):
-    def __init__(self, master):
-        customtkinter.CTkFrame.__init__(self, master)
-        
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        
-        self.settings_menu_button = customtkinter.CTkButton(self, command=lambda: master.switch_view(SettingsPageFrame), text="", width=20, height=20,
-                                                         image=customtkinter.CTkImage(light_image=Image.open("assets/gear.jpg"),
-                                                                                     dark_image=Image.open("assets/gear.jpg")),
-                                                                                     fg_color=None)
-        self.settings_menu_button.grid(row=2, column=0, padx=20, pady=20, sticky="sw")
-        
-        self.button1 = customtkinter.CTkButton(self, text="Open page one", command=lambda: master.switch_view(TerminalPageFrame))
-        self.button1.grid(row=0, column=1, padx=20, pady=8)
-        
-        self.button2 = customtkinter.CTkButton(self, text="Open page two", command=lambda: master.switch_view(SettingsPageFrame))
-        self.button2.grid(row=1, column=1, padx=20, pady=8)
+# Toggles the password between hidden and shown on LoginPage.
+def toggle_hidden_login(app_instance):
+    if app_instance.pass_entry.cget("show") == '':
+        app_instance.pass_entry.configure(show='*')
+        app_instance.hide_pass_button.configure(image=customtkinter.CTkImage(light_image=Image.open("assets/hidden_white.png"),
+                                                                             dark_image=Image.open("assets/hidden_black.png")))
+    else:
+        app_instance.pass_entry.configure(show='')
+        app_instance.hide_pass_button.configure(image=customtkinter.CTkImage(light_image=Image.open("assets/open_white.png"),
+                                                                             dark_image=Image.open("assets/open_white.png")))
 
 
-app = App()
-app.mainloop()
+# Toggles the password between hidden and shown on AccountCreationPage.
+def toggle_hidden_creation(app_instance):
+    if app_instance.pass_entry.cget("show") == '':
+        app_instance.pass_entry.configure(show='*')
+        app_instance.pass_ver_entry.configure(show='*')
+        app_instance.hide_pass_button.configure(image=customtkinter.CTkImage(light_image=Image.open("assets/hidden_white.png"),
+                                                                             dark_image=Image.open("assets/hidden_black.png")))
+    else:
+        app_instance.pass_entry.configure(show='')
+        app_instance.pass_ver_entry.configure(show='')
+        app_instance.hide_pass_button.configure(image=customtkinter.CTkImage(light_image=Image.open("assets/open_white.png"),
+                                                                             dark_image=Image.open("assets/open_white.png")))
+
+
+def password_verification(app_instance):
+    # TODO
+    # Make an error appear rather than outright disabling the button.
+    
+    # Horrid line of code, wish I could wrap the text.
+    if app_instance.password.get() == app_instance.ver_password.get() and app_instance.username.get() != "" and app_instance.password.get() != "" and app_instance.ver_password.get() != "" and len(app_instance.username.get()) > 5  and len(app_instance.password.get()) > 5  and len(app_instance.ver_password.get()) > 5:
+        app_instance.create_account_button.configure(state="normal")
+    else:
+        app_instance.create_account_button.configure(state="disabled")
